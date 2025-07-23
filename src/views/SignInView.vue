5f6bb9ea64194276d2cdaa65b8d00053dff42ed2
hzokbe <hzokbe@gmail.com>
2025-07-23 13:14:33 -0300

    feat(views): add sign in form and submit handler

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,8 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref('');
+
+const password = ref('');
+
+const submit = async () => {
+  console.log('submit');
+
+  console.log(`'${username.value}'`);
+
+  console.log(`'${password.value}'`);
+};
+</script>
 
 <template>
   <div>
     <h1>Sign In</h1>
+
+    <form @submit.prevent="submit">
+      <input type="text" placeholder="Username" required v-model="username" />
+
+      <input type="password" placeholder="Password" required v-model="password" />
+
+      <button type="submit">Sign In</button>
+    </form>
   </div>
 </template>
 
