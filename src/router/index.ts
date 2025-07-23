ff38433f67b5cca0f99474fb050edfd2af0a7d59
hzokbe <hzokbe@gmail.com>
2025-07-23 13:05:48 -0300

    feat(router): add /sign-in route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import HomeView from '@/views/HomeView.vue';
 
 import SignUpView from '@/views/SignUpView.vue';
 
+import SignInView from '@/views/SignInView.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +18,11 @@ const router = createRouter({
       name: 'Sign Up',
       path: '/sign-up',
       component: SignUpView,
+    },
+    {
+      name: 'Sign In',
+      path: '/sign-in',
+      component: SignInView,
     },
   ],
 });
