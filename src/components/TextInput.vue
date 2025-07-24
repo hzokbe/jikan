aafd8cf1465f7ff3ac917829f73a498f4b640d38
hzokbe <hzokbe@gmail.com>
2025-07-24 16:03:23 -0300

    feat(components): add TextInput component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{ placeholder: string; modelValue: string }>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void;
+}>();
+
+import PersonIcon from '@/assets/person.svg';
+</script>
+
+<template>
+  <div>
+    <img :src="PersonIcon" />
+
+    <input
+      type="text"
+      :placeholder="placeholder"
+      required
+      @change="(e) => emit('update:modelValue', (e.target as HTMLInputElement).value)"
+    />
+  </div>
+</template>
+
+<style scoped></style>
