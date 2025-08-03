d2fc6710e4dcc00f0863d8582aad49f7bdc77795
hzokbe <hzokbe@gmail.com>
2025-08-03 16:42:38 -0300

    feat(components): add ToastNotification component

---
+++ src/components/ToastNotification.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import InfoIcon from '@/assets/info.svg';
+
+import WarningIcon from '@/assets/warning.svg';
+
+import ErrorIcon from '@/assets/error.svg';
+
+import { computed } from 'vue';
+
+const props = defineProps<{ message: string; type: 'info' | 'warning' | 'error' }>();
+
+const icon = computed(() => {
+  switch (props.type) {
+    case 'info':
+      return InfoIcon;
+    case 'warning':
+      return WarningIcon;
+    case 'error':
+    default:
+      return ErrorIcon;
+  }
+});
+</script>
+
+<template>
+  <div>
+    <img :src="icon" alt="Toast Notification Icon" />
+
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped></style>
