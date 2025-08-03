34288b19ffe7c2263f4dbfd4bcc291b684b80b89
hzokbe <hzokbe@gmail.com>
2025-08-03 16:33:32 -0300

    feat(sign-up): update submit function

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -16,14 +16,6 @@ const password = ref('');
 const passwordConfirmation = ref('');
 
 const submit = async () => {
-  console.log('submit');
-
-  console.log(`'${username.value}'`);
-
-  console.log(`'${password.value}'`);
-
-  console.log(`'${passwordConfirmation.value}'`);
-
   if (password.value != passwordConfirmation.value) {
     console.error('invalid password confirmation');
 
