4880289f5b716b64060f6cc592b2a28317d36ff7
hzokbe <hzokbe@gmail.com>
2025-08-06 20:08:03 -0300

    refactor(sign-in): extract sign in to a component

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,106 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-import PasswordInput from '@/components/PasswordInput.vue';
-
-import TextInput from '@/components/TextInput.vue';
-
-import api from '@/api/api';
-
-import { AxiosError } from 'axios';
-
-import ToastNotification from '@/components/ToastNotification.vue';
-
-import { useRouter } from 'vue-router';
-
-const username = ref('');
-
-const password = ref('');
-
-const showToastNotification = ref(false);
-
-const toastNotificationMessage = ref('');
-
-const toastNotificationType = ref<'info' | 'warning' | 'error' | 'success'>('info');
-
-const router = useRouter();
-
-const submitButtonText = ref('Sign In');
-
-const isSubmitButtonDisabled = ref(false);
-
-const submit = async () => {
-  showToastNotification.value = false;
-
-  try {
-    submitButtonText.value = 'Loading...';
-
-    isSubmitButtonDisabled.value = true;
-
-    const basicToken = btoa(`${username.value}:${password.value}`);
-
-    await api.post(
-      '/sign-in',
-      {},
-      {
-        headers: {
-          Authorization: `Basic ${basicToken}`,
-        },
-      },
-    );
-
-    showToastNotification.value = true;
-
-    toastNotificationMessage.value = 'Successfully signed in';
-
-    toastNotificationType.value = 'success';
-
-    router.push('/');
-  } catch (err) {
-    showToastNotification.value = true;
-
-    toastNotificationType.value = 'error';
-
-    if (err instanceof AxiosError) {
-      const response = err.response;
-
-      if (response) {
-        const status = response?.status;
-
-        if (status == 401) {
-          toastNotificationMessage.value = 'Wrong username or password';
-        }
-      } else {
-        toastNotificationMessage.value = 'Network error';
-      }
-    }
-  }
-
-  isSubmitButtonDisabled.value = false;
-
-  submitButtonText.value = 'Sign In';
-};
+import SignInForm from '@/components/SignInForm.vue';
 </script>
 
 <template>
   <div>
-    <h1>Sign In</h1>
-
-    <form class="form" @submit.prevent="submit">
-      <TextInput placeholder="Username" v-model="username" />
-
-      <PasswordInput placeholder="Password" v-model="password" />
-
-      <p>Don't have an account? <a href="/sign-up">Sign Up</a></p>
-
-      <button type="submit" :disabled="isSubmitButtonDisabled">{{ submitButtonText }}</button>
-    </form>
-
-    <ToastNotification
-      :message="toastNotificationMessage"
-      :type="toastNotificationType"
-      v-if="showToastNotification"
-    />
+    <SignInForm />
   </div>
 </template>
 
